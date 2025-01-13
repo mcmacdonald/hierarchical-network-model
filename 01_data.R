2470cd598f7cd99f5ed2244c55323f076a1c29d9
Mitch Macdonald <35157616+mcmacdonald@users.noreply.github.com>
2025-01-12 20:57:45 -0700

    01_data.R

--- 01_data.R
+++ 01_data.R
@@ -21,9 +21,34 @@ import = function(file){
   data = data[-1, -1] # drop first row, column (i.e, vertex labels)
   return(data)
 }
-# import drug trafficking networks
-caviar = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/CAVIAR_FULL.csv") # caviar drug trafficking network - Morselli
-heroin = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/HEROIN_DEALING.csv") # NY heroin trafficking network - Natarajan
+siren    = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/CAVIAR_FULL.csv") # siren auto theft
+togo     = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/TOGO.csv") # togo auto theft
+caviar   = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/CAVIAR_FULL.csv") # caviar drug trafficking network - Morselli
+cocaine  = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/COCAINE_DEALING.csv") # NY cocaine trafficking network - Natarajan
+heroin   = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/HEROIN_DEALING.csv") # NY heroin trafficking network - Natarajan
+cielnet  = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/CIELNET.csv") # CielNet (Montreal drug runner) network - Morselli
+italian  = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/ITALIAN_GANGS.csv") # Italian gangs
+london   = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/LONDON_GANG.csv") # London gangs
+montreal = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/MONTREALGANG.csv") # Montreal gangs
+infinito = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-networks/master/data/NDRANGHETAMAFIA_2M.csv") # Ndrangheta meeting participation i.e., Operation Infinito
+
+# transpose the infinto meeting network from bipartite into unipartite graph
+infinito = igraph::graph.incidence(
+  incidence = infinito, # groups i.e., meeting attendance
+  directed = FALSE,
+  mode = "total",
+  weighted = NULL
+  )
+
+# bipariate projection of the incidence/event two-mode network
+infinito = igraph::bipartite.projection(graph = infinito)
+infinito = m_infinito$proj1 # keep the unipartite projection
+infinito = igraph::get.adjacency( # convert graph to adj matrix
+  graph = infinito,
+  type = "both"
+  ) 
+infinito = as.matrix(infinito)     # into matrix format
+infinito = as.data.frame(infinito) # into data frame format
 
 
 
@@ -34,6 +59,11 @@ heroin = import(file = "https://raw.githubusercontent.com/mcmacdonald/criminal-n
 # 2. transform into binary adjacency matrix, or adj matrix with no edge weights
 # 3. transform into symmetric edgelist
 symmetrize = function(mat){
+
+  # required packages
+  require("igraph")
+  
+  # matrix manipulation
   mat[is.na(mat)] <- 0 # set missing cells = 0 i.e., no tie (in case of missings)
   mat = as.matrix(mat) # coerce to matrix
   mat = igraph::graph_from_adjacency_matrix( # adj matrix
@@ -67,7 +97,15 @@ symmetrize = function(mat){
 }
 
 # symmetrize
+siren    = symmetrize(siren)
+togo     = symmetrize(togo)
 caviar   = symmetrize(caviar)
+cocaine  = symmetrize(cocaine)
 heroin   = symmetrize(heroin)
+cielnet  = symmetrize(cielnet)
+italian  = symmetrize(italian)
+london   = symmetrize(london)
+montreal = symmetrize(montreal)
+infinito = symmetrize(infinito)
 
 # ... close .R script
